c6fd9c008b1794803eb0dec198568f72e4a733de
Naeem847 <ashoornaeem@gmail.com>
2025-08-24 13:24:17 +0500

    Create conditional.py
    Add age check program using if-else and while loop
    
    Practice Python conditionals with CNIC age example
    
    Created simple program for age verification with exit option
    
    Add loop to take user input until exit

---
+++ conditional.py
@@ -0,0 +1,24 @@
+
+age=18
+if age<18:
+ print("now are under 18")
+elif age==18:
+   print("yes you can get CNIC from nadra")
+#  enter the user input uses the while loop for practise
+while True:
+ age=int(input("enter your age:\n"))
+ if age==0:
+   print("good bye")
+   break
+
+ elif age<18:
+    print("sorry you are under 18")
+ elif age==18:
+    print("yes you can get the CNIC from the nadra")
+ elif age>18:
+    print("you should have cnic card")
+
+    
+
+
+   
